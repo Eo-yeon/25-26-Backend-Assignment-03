6048b1ba677aa028c35d7d037a4843e00b3706eb
kimeoyeon <djdus126@gmail.com>
2025-11-02 19:20:44 +0900

    feat: JpaPost 생성, ERD 및 Postman 실행결과

---
+++ 김어연/JpaPost/src/main/java/com/gdg/jpapost/domain/Post.java
@@ -0,0 +1,38 @@
+package com.gdg.jpapost.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Post(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+
+    public void update(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+}
---
+++ 김어연/JpaPost/src/main/java/com/gdg/jpapost/domain/User.java
@@ -0,0 +1,34 @@
+package com.gdg.jpapost.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+    @Builder
+    public User(Long id, String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+}
